f624c9f73bf88322f1bfe78642fe6b305d7843eb
0nlyy._alxn <70292271+Trollmaster847@users.noreply.github.com>
2022-07-04 18:09:55 -0600

    Update UI_Loader.lua

--- Utilities/UI_Loader.lua
+++ Utilities/UI_Loader.lua
@@ -3,12 +3,6 @@ local launcherData = game:GetService("HttpService"):JSONDecode((game:HttpGet("ht
 local statusData = launcherData["StatusData"];
 local latestVersion = launcherData["LatestVersion"];
 
-print(statusData["InfinityHub"], statusData["InfinityHub_API"], statusData["InfinityHub_Launcher"])
-
-for i, v in pairs(statusData) do
-    print(i..":", v)
-end
-
 if statusData["InfinityHub_API"] == "Offline" or statusData["InfinityHub"] == "Offline" or statusData["InfinityHub_Launcher"] == "Offline" then
 	spawn(function()
         game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Error", Text = "An error ocurred trying to load the hub!, Please try again later",Button1 = "OK", Duration = 25;})
