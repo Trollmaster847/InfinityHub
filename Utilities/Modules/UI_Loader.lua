9ff6bc31d500439d06072ba439c63ef7ad15d8ea
0nlyy._alxn <70292271+Trollmaster847@users.noreply.github.com>
2022-07-22 11:08:43 -0600

    Add files via upload

---
+++ Utilities/Modules/UI_Loader.lua
@@ -0,0 +1,13 @@
+local launcherData = game:GetService("HttpService"):JSONDecode((game:HttpGet("https://raw.githubusercontent.com/Trollmaster847/InfinityHub/main/Utilities/Modules/Hub_MetaData.json")));
+
+local statusData = launcherData["StatusData"];
+local latestVersion = launcherData["LatestVersion"];
+
+if statusData["InfinityHub_API"] == "Offline" or statusData["InfinityHub"] == "Offline" or statusData["InfinityHub_Launcher"] == "Offline" then
+	spawn(function()
+        game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Error", Text = "An error ocurred trying to load the hub!, Please try again later",Button1 = "OK", Duration = 25;})
+	end)
+	return
+end
+
+loadstring(game:HttpGet(latestVersion))();
